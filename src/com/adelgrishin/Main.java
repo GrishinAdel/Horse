b39ed052380790d46cc47dabb42b8d7ce5bb361f
Adel Grishin <adelgrishin@gmail.com>
2022-04-28 10:27:21 +0300

    add time-work

--- src/com/adelgrishin/Main.java
+++ src/com/adelgrishin/Main.java
@@ -7,21 +7,26 @@ public class Main {
     static int x_change[] = new int[] {1,2,2,1,-1,-2,-2,-1};
     static int y_change[] = new int[] {2,1,-1,-2,-2,-1,1,2};
 
+
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
-        int n = 8;
+        int n = 6;
         int matrix [][] = new int[n][n];
         for (int i = 0; i<n; i++) {
             for (int j = 0; j<n; j++) {
                 matrix[i][j] = 0;
             }
         }
+        System.out.println("Введите значение строки: ");
         int x1 = in.nextInt();
+        System.out.println("Введите значение столбца: ");
         int y1 = in.nextInt();
-        motion(matrix,1, x1--, y1--, n);
+        long time = System.currentTimeMillis();
+        motion(matrix,1, --x1, --y1, n,time);
     }
 
-    static void motion(int matrix[][], int num, int x1, int y1, int n) {
+    static void motion(int matrix[][], int num, int x1, int y1, int n, long time) {
         matrix[x1][y1] = num++;
         for (int i = 0; i < 8; i++ ) {
             int x_new = x1 + x_change[i];
@@ -29,16 +34,15 @@ public class Main {
 
             if (num > n*n) {
                 print(matrix, n);
+                System.out.println(System.currentTimeMillis() - time);
                 System.exit(0);
             }
             if (x_new < 0 || x_new > n-1 || y_new < 0 || y_new > n -1 || matrix[x_new][y_new] != 0) {
                 continue;
             }
-            motion(matrix, num, x_new, y_new, n);
+            motion(matrix, num, x_new, y_new, n, time);
             matrix[x_new][y_new] = 0;
         }
-
-
     }
 
     static void print(int matrix[][], int n) {
